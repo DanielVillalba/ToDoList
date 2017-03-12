1600d681eeaceaf6a9260a87f5c5e4058f42319a
Daniel Villalba <dvillalba@tiempodevelopment.com>
2017-03-12 13:04:11 -0600

    Adding basic repository pattern to the TDList entity

---
+++ ToDoList/DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.DataAccess.Repositories
+{
+    //The Generic Interface Repository for Performing Read/Add/Delete operations
+    public interface IRepository<TEntity, in TKey> where TEntity : class
+    {
+        IEnumerable<TEntity> Get();
+        TEntity Get(TKey id);
+        void Add(TKey id, TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+    }
+}
---
+++ ToDoList/DataAccess/Repositories/TDListRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ToDoList.Models;
+
+namespace ToDoList.DataAccess.Repositories
+{
+    public class TDListRepository : IRepository<TDList, int>
+    {
+        public void Add(int id, TDList entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TDList> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TDList Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TDList entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TDList entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
