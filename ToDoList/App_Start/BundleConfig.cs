e94dae04fce1dd3af5287639d2daa0c931ea7833
Daniel Villalba <dvillalba@tiempodevelopment.com>
2017-03-13 09:22:43 -0600

    Adding AngularJS framework

--- ToDoList/App_Start/BundleConfig.cs
+++ ToDoList/App_Start/BundleConfig.cs
@@ -8,6 +8,11 @@ namespace ToDoList
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.js",
+                        "~/Scripts/angular-route.js",
+                        "~/Scripts/ApplicationScripts/app.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
